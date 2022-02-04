96f40a5d7bda8a6007cd3b4e8a2692025cde5186
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2022-02-04 17:20:19 -0500

    update

--- driver.cpp
+++ driver.cpp
@@ -1,32 +1,50 @@
 #include "bucket_list.h"
+#include "noisy_allocator.h"
+#include "compressed_pair.h"
 
 #include <iostream>
 #include <vector>
+#include <list>
+#include <cstdint>
 
-int main() {
-    std::vector<int> vec1;
-    bucket_list<int> list;
+using i64 = int64_t;
 
-    std::cout << sizeof(vec1);
-    std::cout << '\n';
-    std::cout << sizeof(list);
-    std::cout << '\n';
-
-    for(int i = 0; i < 100; ++i) {
-        //vec1.push_back(i);
-        list.push_back(i);
+struct Test {
+    i64 num;
+    Test(i64 i) {
+        num = i;
     }
 
+};
+
+int main() {
+{
+    noisy_allocator<>::set_granularity(log_granularity::all);
+
+    std::vector<Test, noisy_allocator<Test>> vec;
+    //std::list<Test, noisy_allocator<Test>> stdlist;
+    bucket_list<Test, 32, noisy_allocator<Test>> list;
+
+    //std::cout << "size of vector: " << sizeof(vec) << "\n";
+    //std::cout << "size of stdlist: " << sizeof(stdlist) << "\n";
+    //std::cout << "size of bucket_list: " << sizeof(list) << "\n";
+
+    for(int i = 0; i < 100; ++i) {
+        vec.push_back(Test(i));
+        //stdlist.push_back(Test(i));
+        list.push_back(Test(i));
+    }
 
 /*
     for(int i = 0; i < 100; ++i) {
-        std::cout << vec1[i] << ', ';
+        std::cout << vec[i].num << ", ";
     }
-
     std::cout << '\n';
-*/
 
     for(int i = 0; i < 100; ++i) {
-        std::cout << list[i] << ", ";
+        std::cout << list[i].num << ", ";
     }
+*/
+}
+    noisy_allocator<>::get_report();
 }
